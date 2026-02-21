a0e69c59e3735f1050e77d8419983814efa0b733
Sub <maxim.k.elasnce@gmail.com>
2026-02-21 10:57:47 +0300

    draw curves

--- testForCadexch/visual/window.cpp
+++ testForCadexch/visual/window.cpp
@@ -1,8 +1,30 @@
+#include <numbers>
 #include "window.h"
 #include "raylib.h"
 
 const int SCREEN_WIDTH = 1200;
 const int SCREEN_HEIGHT = 800;
+
+void drawCurve(const std::shared_ptr<Curve>& curve, Color color)
+{
+    const int segments = 50;
+    const double maxT = 4.0 * std::numbers::pi;
+
+    for (int i = 0; i < segments; ++i)
+    {
+        double t1 = maxT * i / segments;
+        double t2 = maxT * (i + 1) / segments;
+
+        auto p1 = curve->getPoint(t1);
+        auto p2 = curve->getPoint(t2);
+
+        DrawLine3D(
+            { (float)p1[0], (float)p1[2], (float)p1[1] },
+            { (float)p2[0], (float)p2[2], (float)p2[1] },
+            color
+        );
+    }
+}
 
 void openVisualWindow(const std::vector<std::shared_ptr<Curve>>& curvesContainer, std::string log)
 {
@@ -31,10 +53,11 @@ void openVisualWindow(const std::vector<std::shared_ptr<Curve>>& curvesContainer
 
         DrawGrid(20, 1.0f);
 
-        // TODO:
-        // Draw graphic here
+        for (const auto& curve : curvesContainer)
+            drawCurve(curve, RED);
 
         EndMode3D();
+
         EndDrawing();
     }
     CloseWindow();
